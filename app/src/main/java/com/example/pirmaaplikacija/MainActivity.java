3c1dbd17ac4a5806150b77c21cc2d2cb7af32f85
Gedvydas Kleberis <gedvtdas.kleberis@gmail.com>
2024-10-09 20:00:42 +0300

    Revert "Added functions to the button"
    This reverts commit 2d73cc6bbbd77ef9b7c424c8ddbabc2b88db4c03.

--- app/src/main/java/com/example/pirmaaplikacija/MainActivity.java
+++ app/src/main/java/com/example/pirmaaplikacija/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.pirmaaplikacija;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,17 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        TextView myTextView = findViewById(R.id.textView3);
-        Button buttonChangeText = findViewById(R.id.button2);
-
-
-        buttonChangeText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myTextView.setText("Goodbye");
-            }
         });
     }
 }
